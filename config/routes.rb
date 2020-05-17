6aa8a1726a8fe155e39d1cfc7df9d89fddc55b16
shubhamtaywade82 <shubhamtaywade82@gmail.com>
2020-05-17 20:02:45 +0530

    added edit and update feature to the app

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
 
   get 'about', to: 'pages#about'
 
-  resources :articles, only: %i[show index]
+  resources :articles, only: %i[show index new create edit update]
 end
