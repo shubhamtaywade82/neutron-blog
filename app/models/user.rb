e1e36cf68368777ece10016af89e3ca12a92a54d
shubhamtaywade82 <shubhamtaywade82@gmail.com>
2020-05-22 16:23:37 +0530

    used before save method to alter object state smallcasethe email field

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@
 
 # userclass
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   has_many :articles
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :username, presence: true,
