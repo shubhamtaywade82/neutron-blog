7c15e2286b3c99b775d76b3dd339fe11989b3e23
shubhamtaywade82 <shubhamtaywade82@gmail.com>
2020-05-25 00:38:02 +0530

    production deploy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -57,7 +57,7 @@ class UsersController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @user && !current_user
+    if current_user != @user && !current_user.admin?
       flash[:alert] = 'You can only edit or delete your own profile'
       redirect_to @user
     end
